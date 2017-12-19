4c5379f4469eca5350c7b4201c6eae36c0aded4f
Forfarming <forfarming@ForfarmingdeiMac.local>
2017-12-19 14:22:43 +0800

    Initial Commit

---
+++ RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by  Forfarming on 2017/12/19.
+//  Copyright © 2017年  Forfarming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
